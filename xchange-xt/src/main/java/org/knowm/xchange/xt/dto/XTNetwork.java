f474728db84d8f0c66264c957ad78af44b617662
konbluesky <konbluesky@163.com>
2024-06-13 16:30:24 +0800

    add XTNetwork

---
+++ xchange-xt/src/main/java/org/knowm/xchange/xt/dto/XTNetwork.java
@@ -0,0 +1,33 @@
+package org.knowm.xchange.xt.dto;
+
+import lombok.Getter;
+import lombok.ToString;
+
+/**
+ * <p> @Date : 2024/6/13 </p>
+ * <p> @Project : XChange</p>
+ *
+ * <p> @author konbluesky </p>
+ */
+@ToString
+@Getter
+public class XTNetwork {
+
+  public static final String BNB_SMART_CHAIN = "BNB Smart Chain";
+  public static final String ETHEREUM = "Ethereum";
+  public static final String POLYGON = "Polygon";
+  public static final String SOL_SOL = "SOL-SOL";
+  public static final String BITCOIN = "Bitcoin";
+  public static final String FANTOM = "Fantom";
+  public static final String CFX = "CFX";
+  public static final String XT_SMART_CHAIN = "XT Smart Chain";
+  public static final String OPBNB = "OPBNB";
+  public static final String TRON = "Tron";
+  public static final String AVAX_C_CHAIN = "AVAX C-Chain";
+  public static final String ARB = "ARB";
+  public static final String OPT = "OPT";
+  public static final String GATECHAIN = "GateChain";
+  public static final String BRISE = "BRISE";
+
+
+}
